f29626d611ee930acf44fbff4f4c3a9220ac0b79
drew-loukusa <unconfigured@null.spigotmc.org>
2018-05-20 23:58:41 -0700

    Small note added in Airport_Simulation.py

--- Airport_Simulation/Airport_Simulation/Airport_Simulation.py
+++ Airport_Simulation/Airport_Simulation/Airport_Simulation.py
@@ -1,1 +1,7 @@
 
+
+#==================================== NOTE ========================================#
+
+	# Biggest thing we have to figure out is how big is each grid cell going to be.
+	# We need to overlay a grid onto our image so we can start assigning coords to 
+	# the terminals. 
