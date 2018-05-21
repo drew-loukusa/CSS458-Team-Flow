9ae5b397a93642d184777b149fb677c227f6fbbf
drew-loukusa <dlwerd@gmail.com>
2018-05-21 00:01:35 -0700

    Just pushing to see if my email address shows up properly now.

--- Airport_Simulation/Airport_Simulation/Airport_Simulation.py
+++ Airport_Simulation/Airport_Simulation/Airport_Simulation.py
@@ -5,3 +5,5 @@
 	# Biggest thing we have to figure out is how big is each grid cell going to be.
 	# We need to overlay a grid onto our image so we can start assigning coords to 
 	# the terminals. 
+
+	#Will do this tommorrow: 5/21/18
